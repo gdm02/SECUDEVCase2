23301160fe4f2f1cf7b88c0db07cde235a0e29bb
Gabriel del Mundo <gabriel_delmundo@dlsu.edu.ph>
2015-09-13 11:05:29 +0800

    Daryll commits

---
+++ secudevcase2/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form>
+	Username: <input type="text">
+	Password: <input type="password">	
+	<input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html>
