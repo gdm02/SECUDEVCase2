502c40e8690c77f6b96b760fa633bbecd26fd2d6
daryllhu <daryllhu@gmail.com>
2015-09-13 14:21:39 +0800

    2
    2

--- secudevcase2/register.php
+++ secudevcase2/register.php
@@ -19,8 +19,34 @@
 					//$firstname = $_POST["firstname"];
 					echo "firstnamevalid";
 				} else {
-					echo "invalid";
+					echo "invalid First Name";
 					//$firstnameErr = "Only letters and white space allowed"; 
+				}
+			}
+			if (empty($_POST["lastname"])){
+				$firstnameErr = "Last name must be supplied.";
+			} else {
+				//if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+				//if (!preg_match("/^[a-zA-Z0-9 ]*$/", $firstname)) {
+				if (preg_match("/^[a-zA-Z]+$/", $_POST["lastname"])) {
+					//$firstname = $_POST["firstname"];
+					echo "lastnamevalid";
+				} else {
+					echo "invalid Last Name";
+					//$firstnameErr = "Only letters and white space allowed";
+				}
+			}
+			if (empty($_POST["username"])){
+				$firstnameErr = "Username must be supplied.";
+			} else {
+				//if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+				//if (!preg_match("/^[a-zA-Z0-9 ]*$/", $firstname)) {
+				if (preg_match("/^[a-zA-Z_]+$/", $_POST["username"])) {
+					//$firstname = $_POST["firstname"];
+					echo "usernamevalid";
+				} else {
+					echo "invalid Username";
+					//$firstnameErr = "Only letters and white space allowed";
 				}
 			}
 		}
@@ -37,6 +63,29 @@
 		<label>Firstname: </label>
 		<input type = "text" name = "firstname" maxlength = '50'/>
 		<br><br>
+		<label>Lastname: </label>
+		<input type = "text" name = "lastname" maxlength = '50'/>
+		<br><br>
+		<label>Gender: </label>
+		<select name="gender">
+   			<option value = "male" >Male</option>
+   			<option value = "female" >Female</option>
+   		</select>
+   		<br><br>
+   		<label>Salutation: </label>
+   		<br><br>
+   		<label>Birthdate: </label>
+   		<input type = "date" name = "birthdate">
+   		<br><br>
+   		<label>Username: </label>
+   		<input type = "text" name = "username" maxlength = '50'/>
+   		<br><br>
+   		<label>Password: </label>
+   		<input type = "password" name = "password">
+   		<br><br>
+   		<label>About Me: </label>
+   		<textarea name="comment" rows="5" cols="40"> </textarea>
+   		<br><br>
 		<input type = "submit" value = "Submit"/> 
 	</form>
 </body>
