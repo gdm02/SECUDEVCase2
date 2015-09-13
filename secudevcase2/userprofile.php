fa4c7859772622f7dd25f50c1af13b3b480911cf
chesteribarrientos <retxhellboy@gmail.com>
2015-09-14 01:17:00 +0800

    minor commit

--- secudevcase2/userprofile.php
+++ secudevcase2/userprofile.php
@@ -3,36 +3,44 @@ session_start();
 include 'connect.php';
 if(isset($_REQUEST['userprofile']) && isset($_SESSION['signed_in']) && $_SESSION['signed_in'] = true)
 {
-	$stmt = $db->prepare("SELECT fname,lname, gender, birthdate, salutation, username, about, level, joindate FROM accounts WHERE username=:username");
-	$stmt->execute(array(':username' => $_REQUEST['userprofile']));
-	
-	while($row = $stmt->fetch(PDO::FETCH_ASSOC))
-	{
-		echo
-			'User profile: <br><label>Firstname: </label>'.
-			$row['fname']
-			.'<br>
-			<label>Surname: </label>'.
-					$row['lname']
+	try{
+		$stmt = $db->prepare("SELECT fname,lname, gender, birthdate, salutation, username, about, level, joindate FROM accounts WHERE username=:username");
+		$stmt->execute(array(':username' => $_REQUEST['userprofile']));
+		if($stmt->rowCount() > 0)
+			while($row = $stmt->fetch(PDO::FETCH_ASSOC))
+			{
+				echo
+					'User profile: <br><label>Firstname: </label>'.
+					$row['fname']
 					.'<br>
-			<label>Username: </label>'.
-					$row['username']
-					.'<br>
-			<label>Gender: </label>'.
-					$row['gender']
-					.'<br>
-			<label>Date Joined:</label>'.
-					$row['joindate']
-					.'<br>
-			<label>Salutation:</label>'.
-					$row['salutation']
-					.'<br>
-			<label>Birthdate: </label>'.
-					$row['birthdate']
-					.'<br>
-			<label>About Me: </label>'.
-					$row['about']
-					.'<br> <br>';
+					<label>Surname: </label>'.
+							$row['lname']
+							.'<br>
+					<label>Username: </label>'.
+							$row['username']
+							.'<br>
+					<label>Gender: </label>'.
+							$row['gender']
+							.'<br>
+					<label>Date Joined:</label>'.
+							$row['joindate']
+							.'<br>
+					<label>Salutation:</label>'.
+							$row['salutation']
+							.'<br>
+					<label>Birthdate: </label>'.
+							$row['birthdate']
+							.'<br>
+					<label>About Me: </label>'.
+							$row['about']
+							.'<br> <br>';
+			}
+		else{
+			echo 'No information can be displayed.';
+		}
+	}
+	catch(PDOException $e){
+		echo 'No information can be displayed.';
 	}
 }
 else{
