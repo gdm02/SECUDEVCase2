d79c47529b0ec9f047855ed6f2f7a775b01a8b1f
Gabriel del Mundo <gabriel_delmundo@dlsu.edu.ph>
2015-09-13 14:24:19 +0800

    base

---
+++ secudevcase2/forum.sql
@@ -0,0 +1,87 @@
+-- MySQL dump 10.13  Distrib 5.6.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: forum
+-- ------------------------------------------------------
+-- Server version	5.6.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(50) DEFAULT NULL,
+  `lname` varchar(50) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `salutation` varchar(50) DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `about` varchar(255) DEFAULT NULL,
+  `level` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+INSERT INTO `accounts` VALUES (1,'cdr','ihd','male','Sir','1995-09-09','admin','1234','default admin account','admin'),(2,'chester','ibarrientos','male','Sir','1994-01-12','rets','1234','because im batman','user'),(3,'daryll','hu','male','Sir','1994-01-12','centhier','1234','i dig chinese chicks','user');
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `acc_id` int(11) NOT NULL,
+  `content` text,
+  `postdate` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `acc_id` (`acc_id`),
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`acc_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-09-13 14:06:29
--- secudevcase2/messageboard.php
+++ secudevcase2/messageboard.php
@@ -1,13 +1,31 @@
 <?php 
 	session_start();
+	//session_unset();
+	//session_destroy();
 ?>
 <!DOCTYPE HTML>
 <html>
 <head>
-	<title>Edit Profile</title>
+	<title>Global Message Boards</title>
 </head>
 <body>
 	<?php
+		class Paginator {
+			private $_conn; //sql connection
+			private $_limit;
+			private $_page;
+			private $_query;
+			private $_total;
+		}
+		
+		function __construct ( $conn, $query ) {
+			$this->_conn = $conn;
+			$this->_query = $query;
+			
+			$rs = $this -> _conn -> query ( $this -> _query );
+			$this -> total = $rs -> num_rows;
+		}
+		// vulnerable?
 		$_SESSION["firstname"] = "Gabriel";
 		$_SESSION["lastname"] = "del Mundo";
 		$_SESSION["gender"] = "Male";
@@ -15,6 +33,7 @@
 		$_SESSION["birthdate"] = "March 16 1996";
 		$_SESSION["aboutme"] = "Hi\nI'm a noob";
 		print_r($_SESSION);
+
 	?>
 	
 	<form method = "POST" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
