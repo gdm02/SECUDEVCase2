9cee5dbebee47129c2653044dac1071b4664dd25
Chester <retxhellboy@gmail.com>
2015-11-11 04:01:09 +0800

    update

--- secudevcase2/shareitem.php
+++ secudevcase2/shareitem.php
@@ -16,9 +16,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 			echo "<form method='POST' class='post-form' action='./submitpost.php'>"
 					."<textarea class = \"form-control\" name='post_content' rows='10' cols = '50' placeholder='Say something about this'></textarea>";
 						
-			echo "<div>" . $row['name'] . "<br>"
-						. $row['price'] . "<br>
-								image here <br>";
+			echo '<div>' . $row['name'] . '<br>'
+						. $row['price'] . '<br>
+							<img src="/'. $row['imgpath'] .'" alt="Item: '. $row['name'].'" style = "width:128px;height:128px"> <br>';
 			echo "<br><input class=\"btn btn-success\" type='submit' name='submit-post' value='Share' /></form>";
 			echo "<button type=\"button\" class=\"btn btn-warning\" onclick = \"history.go(-1);\">Cancel</button>
 			";
